1778d4d90f175e0a79b16da4a4fffbd673929c57
Anton Chernov <36202157+tony-pitchblack@users.noreply.github.com>
2024-12-10 12:57:09 +0300

    Add files via upload

---
+++ server_instructions/ssh_command.sh
@@ -0,0 +1,61 @@
+ssh -i C:\Users\User\.ssh\ano_default_key -l admin 158.160.141.25
+apt install python3.12-venv
+python3 -m venv whisper-live-env
+pip install faster-whisper
+
+cd WhisperLive/
+source whisper-live-env/bin/activate
+python3 run_server.py --port 9090 --backend faster_whisper -fw models/
+
+# no sudo for docker
+grep docker /etc/group
+sudo groupadd docker
+sudo usermod -aG docker admin
+newgrp docker
+
+# faster-whisper-server
+# INSTALL
+sudo apt install docker-buildx
+git clone https://github.com/fedirz/faster-whisper-server.git
+git clone https://github.com/tony-pitchblack/faster-whisper-server
+cd faster-whisper-server
+git checkout truncated-confirmed-sending
+
+# BUILD
+cd faster-whisper-server/ && \
+	sudo docker build -t faster-whisper-server:local -f Dockerfile.cpu . --rm \
+	&& cd ..
+
+# RUN
+sudo docker run \
+  --publish 8000:8000 \
+  --volume ~/.cache/huggingface:/root/.cache/huggingface \
+  --env-file .env \
+  faster-whisper-server:local
+
+# ALIAS
+sudo docker kill $(sudo docker ps -q)
+alias kill_docker='sudo docker kill $(sudo docker ps -q)'
+source ~/.bashrc
+
+# CLEAR INTERMEDIATE FILES
+docker container prune
+docker image prune -a
+
+# DELETE ALL
+docker system prune -af
+
+# EDIT FILES
+vim ~/faster-whisper-server/src/faster_whisper_server/transcriber.py
+vim ~/faster-whisper-server/src/faster_whisper_server/routers/stt.py
+
+# URL QUERY DEBUGGING
+from urllib.parse import parse_qs
+# Access the query parameters from the WebSocket request URL
+query_string = ws.scope.get('query_string').decode('utf-8')
+
+# Print the query string
+print(f"Query String: {query_string}")
+# Parse the query string into a dictionary
+query_params = parse_qs(query_string)
+print(f"Parsed Query Parameters: {query_params}") 
