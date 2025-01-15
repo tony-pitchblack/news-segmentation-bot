187e851ecc3da26b1d500bb147e70cc5326f0bce
Anton Chernov <tony.pitchblack@yandex.ru>
2025-01-15 09:14:18 +0000

    add 1 minute benchmark script for whisper cpp

---
+++ shell_commands/bench_1min.sh
@@ -0,0 +1,24 @@
+# Config
+language="ru"
+model="small"
+
+cd ~/whisper.cpp
+
+# Download model
+./models/download-ggml-model.sh $model
+
+# Get stream url
+source ~/news-segmentation-bot/configs/stream_url.env
+
+# Download and trim the video
+ffmpeg -y -hide_banner -loglevel quiet -i "$STREAM_URL" -t 60 -ac 1 -ar 16000 -acodec pcm_s16le /tmp/whisper-live.wav
+ffprobe /tmp/whisper-live.wav
+
+# Debug whisper-cli
+time ./build/bin/whisper-cli \
+  -t 12 \
+  -m ./models/ggml-${model}.bin \
+  -f /tmp/whisper-live.wav \
+  --language $language \
+  --no-timestamps \
+  # 2> /tmp/whispererr
