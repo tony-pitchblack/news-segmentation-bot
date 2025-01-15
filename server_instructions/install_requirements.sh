ce834fbb0a6eadf5685b6d10d531ab3500016b5f
Anton Chernov <tony.pitchblack@yandex.ru>
2025-01-15 09:58:14 +0000

    add whisper.cpp install instructions

--- server_instructions/install_requirements.sh
+++ server_instructions/install_requirements.sh
@@ -4,14 +4,18 @@
 python3 -m venv news-bot-env
 source news-bot-env/bin/activate
 pip install -r news-segmentation-bot/requirements.txt
+chmod -R 755 ~/news-segmentation-bot
 
 # install shell dependencies
-chmod -R 755 news-segmentation-bot
 sudo ./news-segmentation-bot/scripts/install_selenium.sh
 sudo ./news-segmentation-bot/scripts/install_utils.sh
 
-# install topic-segmentation dependencies
-sudo ./news-segmentation-bot/scripts/install_utils.sh
+# install whisper.cpp
+sudo apt install cmake
+sudo apt install g++
+git clone https://github.com/tony-pitchblack/whisper.cpp/
+
+# install NSE-TopicSegmenation dependencies
 git clone https://github.com/tony-pitchblack/NSE-TopicSegmentation.git nse_topic_segmentation
 pip install -r nse_topic_segmentation/requirements.txt
 
